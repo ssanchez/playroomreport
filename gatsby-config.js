7a97e8b8ea6f3563a6da7887544b86e10925edda
Spencer Sanchez <dev@spencersanchez.com>
2017-12-23 21:14:31 -0500

    Add Contentful to app and main page

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,15 @@
 module.exports = {
-  siteMetadata: {
-    title: `Gatsby Default Starter`,
-  },
-  plugins: [`gatsby-plugin-react-helmet`],
+	siteMetadata: {
+		title: `Gatsby Default Starter`,
+	},
+	plugins: [
+		`gatsby-plugin-react-helmet`,
+		{
+			resolve: `gatsby-source-contentful`,
+			options: {
+				spaceId: process.env.CONTENTFUL_SPACE_ID,
+				accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+			},
+		},
+	],
 }
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,13 +1,42 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { object } from 'prop-types'
 import Link from 'gatsby-link'
 
-const IndexPage = () => (
-	<div>
-		<h1>Hi people</h1>
-		<p>Welcome to your new Gatsby site.</p>
-		<p>Now go build something great.</p>
-		<Link to='/page-2/'>Go to page 2</Link>
-	</div>
-)
+class IndexPage extends Component {
+	render() {
+		const { edges: posts } = this.props.data.allContentfulPost
+		return (
+			<main>
+				{posts.map(({ node: post }) => (
+					<div key={post.id}>
+						<Link to={post.slug}>
+							{post.title.title}
+						</Link>
+					</div>
+				))}
+			</main>
+		)
+	}
+}
+
+IndexPage.propTypes = {
+	data: object,
+}
 
 export default IndexPage
+
+export const pageQuery = graphql`
+	query PageQuery {
+		allContentfulPost {
+			edges {
+				node {
+					id
+					title {
+						title
+					}
+					slug
+				}
+			}
+		}
+	}
+`
